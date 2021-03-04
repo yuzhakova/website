b63aa39a90faa950555bf596251f172b422b4ca4
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2021-03-03 22:44:29 -0800

    built client side using react

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import { Fragment } from "react";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import Container from "react-bootstrap/Container";
+
+
+
+function App() {
+  return (<Fragment>
+    <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-info">Search</Button>
+    </Form>
+  </Navbar>
+
+  <Container>
+  <h1>Hey</h1>
+</Container>
+</Fragment>
+  );
+}
+
+export default App;
