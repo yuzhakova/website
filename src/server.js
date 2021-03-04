fbb7147ea1969bd91a573a309bfe579bfa41c446
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2021-03-03 19:26:02 -0800

    installed nodemon

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-console.log('test')
+const { response } = require('express')
+const express = require('express')
+const application = express()
+
+application.get("/", (request, response) => {
+  response.send('It works')
+})
+
+application.get("/services", (request, response) => response.json([
+  {id: 1, label: "service A"},
+  {id: 2, label: "service B"}
+
+]))
+
+application.listen(3000, () => console.log("Server listening!"))
