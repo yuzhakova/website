76a963ae1f3de0c3d13f038c43c618ad7045a778
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2021-03-07 22:24:05 -0800

    add Header

---
+++ client/src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+// import Typed from 'react-typed';
+
+const Header = () => {
+  return (
+    <div className="header-wraper">
+      <div className="main-info">
+        <h1>business processes and reports optimization</h1>
+
+        {/* <Typed
+          className="typed-text"
+          strings={[
+            'reporting design',
+            'processes optimization',
+            'excel training and automation',
+            'productivity increase',
+          ]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        >
+          <input type="text" />
+        </Typed> */}
+        <a href="#" className="btn-main-offer">
+          contact me
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
