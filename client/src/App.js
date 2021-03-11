ee58818d67684a5e121cedad0155f3aa7ed2eb81
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2021-03-10 19:53:51 -0800

    Applied formatting changes using prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
