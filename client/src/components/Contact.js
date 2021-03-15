84aa48e8b7cbfdd3f2d1f3a2f624ba09b2550a33
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2021-03-15 01:45:30 -0700

    Style Contact component

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -11,47 +11,66 @@ const Contact = () => {
         <div className="row">
           <div className="col-md-6 col-xs-12">
             {/* name input */}
-            <input
-              id="name"
-              type="text"
-              className="form-control"
-              placeholder="Name"
-              name="name"
-            />
+            <div className="text-center">
+              <input
+                id="name"
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                name="name"
+              />
+              <div className="line"></div>
+            </div>
             {/* phone input */}
-            <input
-              id="phone"
-              type="text"
-              className="form-control"
-              placeholder="Phone Number"
-              phone="phone"
-            />
+            <div className="text-center">
+              <input
+                id="phone"
+                type="text"
+                className="form-control"
+                placeholder="Phone Number"
+                phone="phone"
+              />
+              <div className="line"></div>
+            </div>
+
             {/* email input */}
-            <input
-              id="email"
-              type="email"
-              className="form-control"
-              placeholder="Email"
-              email="email"
-            />
+            <div className="text-center">
+              <input
+                id="email"
+                type="email"
+                className="form-control"
+                placeholder="Email"
+                email="email"
+              />
+              <div className="line"></div>
+            </div>
+
             {/* subject input */}
-            <input
-              id="subject"
-              type="text"
-              className="form-control"
-              placeholder="Subject"
-              subject="subject"
-            />
+            <div className="text-center">
+              <input
+                id="subject"
+                type="text"
+                className="form-control"
+                placeholder="Subject"
+                subject="subject"
+              />
+              <div className="line"></div>
+            </div>
           </div>
+
           <div className="col-md-6 col-xs-12">
             {/* description*/}
-            <textarea
-              id="description"
-              type="text"
-              className="form-control"
-              placeholder="Email"
-              email="email"
-            ></textarea>
+            <div className="text-center">
+              <textarea
+                id="description"
+                type="text"
+                className="form-control"
+                placeholder="Enter your message here..."
+                email="email"
+              ></textarea>
+              <div className="line"></div>
+            </div>
+
             <button className="btn-main-offer contact-btn" type="submit">
               contact me
             </button>
