8a4b9bfb28131e252678765d238b7dabee82a752
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2021-03-18 02:14:23 -0700

    Add particles to top part using react-particles-js library

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
@@ -12,6 +13,27 @@ import Contact from "./components/Contact";
 function App() {
   return (
     <>
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "#f9ab00",
+              },
+            },
+          },
+        }}
+      />
       <Navbar />
       <Header />
       <AboutMe />
